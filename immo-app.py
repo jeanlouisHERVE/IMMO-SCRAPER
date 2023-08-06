6b708722248d6697512e163499c0862cd34fd0fb
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-08-06 22:16:45 +0200

    trying to achieve the research section part can't select yet the desired town

--- immo-app.py
+++ immo-app.py
@@ -1,6 +1,5 @@
 #packages
-
-
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,15 +7,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
+#other modules
+from dotenv import load_dotenv
+
 #own packages
 import database
 database.create_table()
 
+#get data from .env file 
+load_dotenv()
 
 #variables
+city_researched="Sar"
 url_immo_website="https://www.seloger.com/vente.htm"
 driver = webdriver.Chrome()
 chrome_options = ChromeOptions()
+city_researched_content = os.environ["CITY_RESEARCHED_CONTENT"]
 
 #functions 
 def check_accept_section(cssSelector: str):
@@ -35,6 +41,13 @@ check_accept_section('span.didomi-continue-without-agreeing')
 driver.implicitly_wait(5)
 
 ##fill research section
+#remove pre selected area "Ile de France"
+driver.find_element(By.CSS_SELECTOR, "div.sc-gLDzan.bevJHu").click()
+driver.find_element(By.CSS_SELECTOR, "input.sc-irTswW.fPqHAw").send_keys(os.environ["CITY_RESEARCHED"])
+driver.implicitly_wait(5)
+xpath_expression = '//span[@value="' + city_researched_content + '"]'
+driver.find_element(By.XPATH, xpath_expression).click()
+#driver.find_element(By.CSS_SELECTOR, "button.sc-bZPPFW.gurgxX").click()
 
 input()
 
