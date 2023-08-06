4c1f176d8bf6ebe98f0f2e0fb4d2a0881d00c6b1
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-08-06 20:18:47 +0200

    add chrome webdriver

--- immo-app.py
+++ immo-app.py
@@ -1,17 +1,26 @@
 #packages
 
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 #own packages
 import database
 database.create_table()
 
+
 #variables
 url_immo_website="https://www.seloger.com/vente.htm"
-
+driver = webdriver.Chrome()
+chrome_options = ChromeOptions()
 
 #functions 
 
 #script
-
+driver.get(url_immo_website)
+input()
 
 database.connection.close()
 driver.close()
