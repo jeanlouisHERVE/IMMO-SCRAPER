482ed25dc0d1230d7791dbb41028ce1769529e83
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-08-06 15:54:00 +0200

    add close dispositions

--- immo-app.py
+++ immo-app.py
@@ -5,7 +5,14 @@ import database
 database.create_table()
 
 #variables
+url_immo_website="https://www.seloger.com/vente.htm"
+
 
 #functions 
 
 #script
+
+
+database.connection.close()
+driver.close()
+driver.quit()
