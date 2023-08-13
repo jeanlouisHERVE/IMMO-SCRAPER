0dfeb2fdf0ae1c33e2a4747942960a8deb93b830
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-08-13 14:53:15 +0200

    add link variable

--- bi-immo-app.py
+++ bi-immo-app.py
@@ -68,12 +68,12 @@ try:
     articles_ready = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.resultsListContainer")))
     if articles_ready:
         print("test OK")
-    articles = driver.find_elements(By.CSS_SELECTOR, "div.sideListItem")
+    articles = driver.find_elements(By.CSS_SELECTOR, "article.sideListItem")
     print("articles",articles)
     for article in articles:
         ###Type of good 
         try:
-            type_of_good_content = driver.find_element(By.CSS_SELECTOR,'span.ad-overview-details__ad-title')
+            type_of_good_content = driver.find_element(By.CSS_SELECTOR,'span.ad-overview-details__ad-title.ad-overview-details__ad-title--small')
             type_of_good_content = type_of_good_content.text
             if "maison" in  type_of_good_content.lower():
                 type_of_good = "maison"
@@ -84,9 +84,11 @@ try:
         except (NoSuchElementException):
             print("KO : no element type_of_good")
         print("type_of_good",type_of_good)
+        
         ###town
-        town = os.environ["CITY_RESEARCHED_CONTENT"]
+        town = os.environ["CITY_RESEARCHED"]
         print("town",town)
+        
         ###District&&Postcode
         try:
             address_content = driver.find_element(By.CSS_SELECTOR,'span.ad-overview-details__address-title')
@@ -96,10 +98,16 @@ try:
             print("district",district)
         except (NoSuchElementException):
             print("KO : no element type_of_good")
-            
-        ###Postcode
-        
+
         ###url
+        try:
+            link_content = driver.find_element(By.CSS_SELECTOR,'a.detailedSheetLink')
+            link = link_content.get_attribute('href')
+            print("link",link)
+        except (NoSuchElementException):
+            print("KO : no element link")
+        
+        
 except (NoSuchElementException):
     print("KO : no annouces found")
 input()
