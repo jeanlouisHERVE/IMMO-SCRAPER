7e50c567a329fae34b96da0cdc28f985f760da62
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-09-04 12:37:30 +0200

    change database location

--- app/driver_setup.py
+++ app/driver_setup.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.common.action_chains import ActionChains
 
 def initialize_driver():
     driver = webdriver.Chrome()
     chrome_options = ChromeOptions()
-    return driver
+    return driver, chrome_options
