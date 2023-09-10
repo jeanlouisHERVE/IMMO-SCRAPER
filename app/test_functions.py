65e26e20cd14abd6bf1aa585593c09d66c7f9884
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-09-10 17:52:50 +0200

    add unit testing to functions module

---
+++ app/test_functions.py
@@ -0,0 +1,45 @@
+import unittest
+from functions import date_converter_french_date_to_utc_timestamp, contains_numbers, are_timestamps_equal
+
+class TestYourFunctions(unittest.TestCase):
+
+    def test_date_converter_french_date_to_utc_timestamp(self):
+        # Test case 1: valid input
+        french_date = "15 août 2023"
+        expected_timestamp = 1689465600.0  # Expected UTC timestamp
+        result = date_converter_french_date_to_utc_timestamp(french_date)
+        self.assertAlmostEqual(result, expected_timestamp, places=2)  # Tolerance of 2 seconds
+        
+        # Test case 2: invalid input with non-existent month
+        french_date = "20 décembrrr 2023"
+        result = date_converter_french_date_to_utc_timestamp(french_date)
+        self.assertIsNone(result)  # Expecting None
+
+    def test_contains_numbers(self):
+        # Test case 1: input contains numbers
+        input_string = "This is a string with 12345 numbers."
+        result = contains_numbers(input_string)
+        self.assertTrue(result)  # Expecting True
+        
+        # Test case 2: input does not contain numbers
+        input_string = "No numbers here."
+        result = contains_numbers(input_string)
+        self.assertFalse(result)  # Expecting False
+
+    def test_are_timestamps_equal(self):
+        # Test case 1: timestamps are equal within the tolerance
+        timestamp1 = 100.0
+        timestamp2 = 101.0
+        tolerance_seconds = 2
+        result = are_timestamps_equal(timestamp1, timestamp2, tolerance_seconds)
+        self.assertTrue(result)  # Expecting True
+        
+        # Test case 2: timestamps are not equal within the tolerance
+        timestamp1 = 100.0
+        timestamp2 = 120.0
+        tolerance_seconds = 10
+        result = are_timestamps_equal(timestamp1, timestamp2, tolerance_seconds)
+        self.assertFalse(result)  # Expecting False
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/tests/test_add_annources.py
@@ -0,0 +1,15 @@
+import unittest
+from add_announces import add_new_announces, add_descriptions
+
+class TestMyModule(unittest.TestCase):
+
+    def test_add_new_announces(self):
+        # Écrire des cas de test pour la fonction add_new_announces
+        # Utilisez les méthodes d'assertion pour vérifier les résultats
+
+    def test_add_descriptions(self):
+        # Écrire des cas de test pour la fonction add_descriptions
+        # Utilisez les méthodes d'assertion pour vérifier les résultats
+
+if __name__ == '__main__':
+    unittest.main()
