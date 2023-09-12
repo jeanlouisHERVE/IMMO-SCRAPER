35446c80d69735e533158da62203e76fffe1d62f
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-09-12 22:27:49 +0200

    lint corrections to app.py

--- app/app.py
+++ app/app.py
@@ -27,7 +27,7 @@ menu_prompt = """-- Menu --
 Enter your choice: """
 
 
-def start_prompt(): 
+def start_prompt():
     while (user_input := input(menu_prompt)) != "5":
         if user_input == "1":
             modules.add_announces.add_new_announces()
@@ -37,10 +37,11 @@ def start_prompt():
             modules.update.update_descriptions()
         elif user_input == "4":
             global_properties_number = modules.database_app.get_properties_number()
-            print(f"""\n--- The properties number --- \n\n The city {city_researched_content} has a total of {global_properties_number[0][0]} properties to sell. \n 
+            print(f"""\n--- The properties number --- \n\n The city {city_researched_content} has a total of {global_properties_number[0][0]} properties to sell. \n
                   """)
         else:
             print("Invalid input, please try again!")
 
 # script
 start_prompt()
+
