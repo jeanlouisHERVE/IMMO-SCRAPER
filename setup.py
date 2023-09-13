dba5000c367c4289a7a06a09771c7843bc906e9b
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-09-13 21:26:13 +0200

    creation of setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='immo-scraper',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 'requests>=2.24.0',
+        # List your project's dependencies here
+    ],
+    scripts=[
+        # List any executable scripts here
+    ],
+    author='Jean-Louis HERVE',
+    author_email='jeanlouis.herve@hotmail.fr',
+    description='Your project description',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
