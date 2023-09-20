963ea33e3ccac58458b2d93b7c87a7711ab5f5a9
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-09-20 23:21:09 +0200

    fix test_05_add_price_to_property

--- tests/test_database_app.py
+++ tests/test_database_app.py
@@ -258,7 +258,7 @@ class TestDatabaseFunctions(unittest.TestCase):
 
         # Check if the retrieved price matches the added data
         self.assertIsNotNone(price_data)
-        self.assertEqual(price_data[0], 2)  # id
+        self.assertEqual(price_data[0], 1)  # id
         self.assertEqual(price_data[1], 1234567890.0)  # date
         self.assertEqual(price_data[2], property_id)  # property_id
         self.assertEqual(price_data[3], 500000)
