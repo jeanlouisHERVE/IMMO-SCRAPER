4bf252db44a310f31342058d8c8ffebd1b069f46
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-09-20 20:07:21 +0200

    create delete_tables function correction

--- modules/database_app.py
+++ modules/database_app.py
@@ -226,16 +226,19 @@ def create_tables():
 
 
 def delete_tables(connection):
-    with connection:
-        print("deleting tables...")
-        connection.execute(DELETE_PROPERTIES_TABLE)
-        connection.execute(DELETE_OLD_PROPERTIES_TABLE)
-        connection.execute(DELETE_PRICES_TABLE)
-        connection.execute(DELETE_OLD_PRICES_TABLE)
-        connection.execute(DELETE_DESCRIPTIONS_TABLE)
-        connection.execute(DELETE_OLD_DESCRIPTIONS_TABLE)
-        connection.execute(DELETE_AGENCIES_TABLE)
-        print("Tables deleted.")
+    try:
+        with connection:
+            print("deleting tables...")
+            connection.execute(DELETE_PROPERTIES_TABLE)
+            connection.execute(DELETE_OLD_PROPERTIES_TABLE)
+            connection.execute(DELETE_PRICES_TABLE)
+            connection.execute(DELETE_OLD_PRICES_TABLE)
+            connection.execute(DELETE_DESCRIPTIONS_TABLE)
+            connection.execute(DELETE_OLD_DESCRIPTIONS_TABLE)
+            connection.execute(DELETE_AGENCIES_TABLE)
+            print("Tables deleted.")
+    except sqlite3.Error as e:
+        print(f"Error deleting tables: {e}")
 
 
 def add_property(
