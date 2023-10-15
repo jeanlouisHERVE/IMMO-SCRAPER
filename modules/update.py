07b0134a86c26b8bc9844ab4da74abed5943f89b
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-10-15 13:42:02 +0200

    fix GET_LAST_PRICE_FOR_PROPRIETY

--- modules/update.py
+++ modules/update.py
@@ -237,6 +237,7 @@ def update_descriptions():
             new_price_content = driver.find_element(By.CSS_SELECTOR, "span.ad-price__the-price").text
             new_price_content = new_price_content.replace(" ", "")
             new_price = re.findall(regex_find_numbers, new_price_content)[0]
+            new_price = float(new_price)
         except NoSuchElementException:
             print("KO : no data for new_price")
 
