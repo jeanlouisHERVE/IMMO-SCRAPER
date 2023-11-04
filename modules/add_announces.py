cb9ff1bd59df35db90deb0a0dc946ec9aab907c8
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-11-04 15:38:37 +0100

    add security to add announce function to avoid infinite loop

--- modules/add_announces.py
+++ modules/add_announces.py
@@ -91,138 +91,141 @@ def add_new_announces():
             print("KO : no more next button")
             break
 
-        articles = driver.find_elements(
-                                By.CSS_SELECTOR, "article.sideListItem"
-                                )
-        print(f"------------------Page_Start {global_page_number-1}------------------")
-        print("articles", articles)
-        for article in articles:
-            print(f"{article} {annonce}")
-            annonce += 1
-            print("------------------Article Start------------------")
-            print("article :", article)
+        try:
+            articles = driver.find_elements(
+                                    By.CSS_SELECTOR, "article.sideListItem"
+                                    )
+            print(f"------------------Page_Start {global_page_number-1}------------------")
+            print("articles", articles)
+            for article in articles:
+                print(f"{article} {annonce}")
+                annonce += 1
+                print("------------------Article Start------------------")
+                print("article :", article)
 
-            # type of property
-            type_of_property = ""
-            try:
-                type_of_property_content = article.find_element(
-                                                    By.CSS_SELECTOR, "span.ad-overview-details__ad-title"
-                                                    )
-                type_of_property_content = type_of_property_content.text
-                if "maison" in type_of_property_content.lower():
-                    type_of_property = "maison"
-                elif "appartement" in type_of_property_content.lower():
-                    type_of_property = "appartement"
-                else:
-                    type_of_property = ""
-                print("type_of_property :", type_of_property)
-
-            except (NoSuchElementException):
-                print("KO : no data for type_of_property found")
-
-            # town
-            town = os.environ["CITY_RESEARCHED"]
-            print("town :", town)
-
-            # District&&Postcode
-            district = ""
-            postcode = 0
-            try:
-                address_content = article.find_element(
-                                        By.CSS_SELECTOR, "span.ad-overview-details__address-title"
-                                        )
-                address_content = address_content.text
-                # district
+                # type of property
+                type_of_property = ""
                 try:
-                    district = re.findall(r"\((.*?)\)", address_content)[0]
-                except IndexError:
-                    print("KO : no data for District found")
-                    district = ""
-                # postcode
+                    type_of_property_content = article.find_element(
+                                                        By.CSS_SELECTOR, "span.ad-overview-details__ad-title"
+                                                        )
+                    type_of_property_content = type_of_property_content.text
+                    if "maison" in type_of_property_content.lower():
+                        type_of_property = "maison"
+                    elif "appartement" in type_of_property_content.lower():
+                        type_of_property = "appartement"
+                    else:
+                        type_of_property = ""
+                    print("type_of_property :", type_of_property)
+
+                except (NoSuchElementException):
+                    print("KO : no data for type_of_property found")
+
+                # town
+                town = os.environ["CITY_RESEARCHED"]
+                print("town :", town)
+
+                # District&&Postcode
+                district = ""
+                postcode = 0
                 try:
-                    postcode = re.findall(r"[0-9]*", address_content)[0]
-                except IndexError:
-                    print("KO : no data for Postcode found")
-                    postcode = 0
-                print("district :", district)
-                print("postcode :", postcode)
+                    address_content = article.find_element(
+                                            By.CSS_SELECTOR, "span.ad-overview-details__address-title"
+                                            )
+                    address_content = address_content.text
+                    # district
+                    try:
+                        district = re.findall(r"\((.*?)\)", address_content)[0]
+                    except IndexError:
+                        print("KO : no data for District found")
+                        district = ""
+                    # postcode
+                    try:
+                        postcode = re.findall(r"[0-9]*", address_content)[0]
+                    except IndexError:
+                        print("KO : no data for Postcode found")
+                        postcode = 0
+                    print("district :", district)
+                    print("postcode :", postcode)
 
-            except (NoSuchElementException):
-                print("KO : no data for District&&Postcode found")
+                except (NoSuchElementException):
+                    print("KO : no data for District&&Postcode found")
 
-            # url
-            url = ""
-            try:
-                url_content = article.find_element(By.CSS_SELECTOR, "a.detailedSheetLink")
-                url = url_content.get_attribute('href')
-                print("link :", url)
-            except (NoSuchElementException):
-                print("KO : no data for url found")
+                # url
+                url = ""
+                try:
+                    url_content = article.find_element(By.CSS_SELECTOR, "a.detailedSheetLink")
+                    url = url_content.get_attribute('href')
+                    print("link :", url)
+                except (NoSuchElementException):
+                    print("KO : no data for url found")
 
-            # room number && surface
-            surface = 0
-            room_number = 0
-            try:
-                room_surface_content = article.find_element(
-                                                    By.CSS_SELECTOR, "span.ad-overview-details__ad-title"
-                                                    )
-                content_text = room_surface_content.text
-                # room
-                room_number = room_surface_content.text
-                pattern_room = r'(\d+)\s*pièce'
-                room_content = re.findall(pattern_room, content_text)
-                room_number = room_content[0]
-                print("room_number :", room_number)
-                # surface
-                surface = room_surface_content.text
-                pattern_squaremeters = r'\b(\d+)\b'
-                surface_content = re.findall(pattern_squaremeters, content_text)
-                surface = surface_content[-1]
-                print("surface :", surface)
-            except (NoSuchElementException):
-                print("KO : no data for room number && surface found")
+                # room number && surface
+                surface = 0
+                room_number = 0
+                try:
+                    room_surface_content = article.find_element(
+                                                        By.CSS_SELECTOR, "span.ad-overview-details__ad-title"
+                                                        )
+                    content_text = room_surface_content.text
+                    # room
+                    room_number = room_surface_content.text
+                    pattern_room = r'(\d+)\s*pièce'
+                    room_content = re.findall(pattern_room, content_text)
+                    room_number = room_content[0]
+                    print("room_number :", room_number)
+                    # surface
+                    surface = room_surface_content.text
+                    pattern_squaremeters = r'\b(\d+)\b'
+                    surface_content = re.findall(pattern_squaremeters, content_text)
+                    surface = surface_content[-1]
+                    print("surface :", surface)
+                except (NoSuchElementException):
+                    print("KO : no data for room number && surface found")
 
-            # price
-            price = 0
-            try:
-                price_content = article.find_element(By.CSS_SELECTOR, "span.ad-price__the-price")
-                price_content = price_content.text
-                price = ''.join(re.findall(r'\d+', price_content))
-                if len(price) > 7:
-                    price = None
-                print("price :", price)
-            except (NoSuchElementException):
-                print("KO : no data for price found")
+                # price
+                price = 0
+                try:
+                    price_content = article.find_element(By.CSS_SELECTOR, "span.ad-price__the-price")
+                    price_content = price_content.text
+                    price = ''.join(re.findall(r'\d+', price_content))
+                    if len(price) > 7:
+                        price = None
+                    print("price :", price)
+                except (NoSuchElementException):
+                    print("KO : no data for price found")
 
-            # date
-            date_add_to_db = current_time_utc
-            print("date_add_to_db :", date_add_to_db)
+                # date
+                date_add_to_db = current_time_utc
+                print("date_add_to_db :", date_add_to_db)
 
-            print("------------------Article End------------------")
+                print("------------------Article End------------------")
 
-            # add properties to db
-            if not database_app.get_property_by_url(url):
-                property_id = database_app.add_property(
-                                                    type_of_property,
-                                                    town,
-                                                    district,
-                                                    postcode,
-                                                    url,
-                                                    room_number,
-                                                    surface,
-                                                    date_add_to_db
-                                                    )
-                database_app.add_price_to_property(date_add_to_db, property_id, price)
+                # add properties to db
+                if not database_app.get_property_by_url(url):
+                    property_id = database_app.add_property(
+                                                        type_of_property,
+                                                        town,
+                                                        district,
+                                                        postcode,
+                                                        url,
+                                                        room_number,
+                                                        surface,
+                                                        date_add_to_db
+                                                        )
+                    database_app.add_price_to_property(date_add_to_db, property_id, price)
 
-        # catch data to access the next page
-        next_page_url = next_results_btn.get_attribute('href')
-        print("next_page_url", next_page_url)
-        pattern_next_page_url_without_page = r"(.+)\?"
-        next_page_url_without_page = re.findall(pattern_next_page_url_without_page, next_page_url)[0]
-        print("next_page_url_without_page :", next_page_url_without_page)
-        driver.get(next_page_url)
-        global_page_number += 1
-        print("------------------Add_new_annouces_End------------------")
+            # catch data to access the next page
+            next_page_url = next_results_btn.get_attribute('href')
+            print("next_page_url", next_page_url)
+            pattern_next_page_url_without_page = r"(.+)\?"
+            next_page_url_without_page = re.findall(pattern_next_page_url_without_page, next_page_url)[0]
+            print("next_page_url_without_page :", next_page_url_without_page)
+            driver.get(next_page_url)
+            global_page_number += 1
+            print("------------------Add_new_annouces_End------------------")
+        except Exception as e:
+            print(f"An error occurred while processing the current page: {e}")
 
 
 def add_descriptions():
